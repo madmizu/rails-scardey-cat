910df9a574c04b3452a306d57e3921c34ddde011
Madison Shimizu <mhshimizu@outlook.com>
2022-02-15 13:11:48 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destorying Existing seed data..."
+User.destroy_all
+Score.destroy_all
+
+puts "Creating User seed data..."
+User.create ({username: "mouse",email: "mouse@email.com",password: "1234"})
+User.create ({username: "cat",email: "cat@email.com",password: "1234"})
+User.create ({username: "dog",email: "dog@email.com",password: "1234"})
+User.create ({username: "bird",email: "bird@email.com",password: "1234"})
+User.create ({username: "fish",email: "fish@email.com",password: "1234"})
+User.create ({username: "cow",email: "cow@email.com",password: "1234"})
+User.create ({username: "deer",email: "deer@email.com",password: "1234"})
+
+puts "Creating Score seed data..."
+20.times do
+    Score.create ({
+        score: Faker::Number.between(from: 100, to: 90000),
+        user_id: Faker::Number.between(from: 1, to: 7)
+    })
+end
+
+puts "Done seeding!"
