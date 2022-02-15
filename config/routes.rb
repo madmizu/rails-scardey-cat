785b09e982c70b5bb9addea643c8f96a44e79d94
Madison Shimizu <mhshimizu@outlook.com>
2022-02-15 13:30:26 -0600

    commented out auths

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :users #all routes
   resources :scores, only:[:index, :create, :show]
 
-  get '/me', to: "users#show"
-  post '/signup', to: "users#create"
-  post '/login', to: "sessions#login"
-  delete '/logout', to: "sessions#logout"
+  # get '/me', to: "users#show"
+  # post '/signup', to: "users#create"
+  # post '/login', to: "sessions#login"
+  # delete '/logout', to: "sessions#logout"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
