b80cc412784461594c4b3147608af2cc8d2bc496
Madison Shimizu <mhshimizu@outlook.com>
2022-02-16 01:08:27 -0600

    adjusted validations for creating new score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -16,6 +16,6 @@ class ScoresController < ApplicationController
 
     private
         def score_params
-            params.permit(:score, :user_id)
+            params.permit(:score, :name, :user_id)
         end
 end
