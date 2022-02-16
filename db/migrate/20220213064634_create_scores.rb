04648556d7355ef2c9675fd67e0d8810ca3d8433
Madison Shimizu <mhshimizu@outlook.com>
2022-02-16 09:06:52 -0600

    changed score migration to null: true for scores

--- db/migrate/20220213064634_create_scores.rb
+++ db/migrate/20220213064634_create_scores.rb
@@ -2,7 +2,7 @@ class CreateScores < ActiveRecord::Migration[6.1]
   def change
     create_table :scores do |t|
       t.integer :score
-      t.belongs_to :user, foregin_key: true
+      t.belongs_to :user, null: true, foregin_key: true
 
       t.timestamps
     end
